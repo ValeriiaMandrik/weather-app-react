63ca2c82447dad7da9470c833e5e70ecf0104354
ValeriiaMandrik <valeriiafareniuk@gmail.com>
2022-10-27 15:10:14 +0100

    created weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Button";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="borders">
+      <div className="Cities">
+        <button className="current" id="currentCity">
+          Current city
+        </button>
+        <Button>Kyiv</Button>
+        <Button>Sumy</Button>
+        <Button>Lisbon</Button>
+        <Button>Cascais</Button>
+      </div>
+      <br />
+      <form className="input-group mb-3">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="City"
+          aria-label="City"
+          aria-describedby="button-addon2"
+        />
+        <input className="search" type="submit" defaultValue="Search" />
+      </form>
+      <div className="container">
+        <div className="row">
+          <div className="col" id="cityValue">
+            City
+          </div>
+        </div>
+        <div className="row">
+          <div className="col" id="dayValue">
+            Day
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <img src="" alt="" id="icon" className="float-left" />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col" id="description">
+            Description
+          </div>
+          <div className="col" id="sunrise">
+            Sunrise
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <span id="tempValue">Temperature</span>
+            <button className="celcius">°C</button>
+            <span>/</span>
+            <button className="farenheit">°F</button>
+          </div>
+          <div className="col" id="sunset">
+            Sunset
+          </div>
+        </div>
+      </div>
+      <table className="table table-bordered" id="forecast" />
     </div>
   );
 }
-
-export default App;
